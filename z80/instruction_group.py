aa6f18a4fe32df932d9169396e3d9a407fcd7e19
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2019-09-27 21:28:45 +0200

    refactored to python3 instruction_group

--- z80/instruction_group.py
+++ z80/instruction_group.py
@@ -1,12 +1,40 @@
+from .instruction_set import InstructionSet
+from .instruction_template import InstructionTemplate
+
+
 class InstructionGroup(object):
-    def __init__(self, name, instruction_set, short_name=None):
+    """
+    A group of Instructions
+    """
+    def __init__(self, name: str,
+                 instruction_set: InstructionSet,
+                 short_name=None):
+        """
+        Constructs a InstructionGroup. Register it self
+        to the InstructionSet
+
+        Parameters
+        ----------
+        name : str
+            The name of the group
+        instruction_set: InstructionSet
+            reference to the full InstructionSet
+        short_name: string, optional
+            The short name of the group, default value: name
+        """
         self.name = name
         self.instruction_templates = []
         self.instruction_set = instruction_set
         self.instruction_set.add_group(self)
         self.short_name = name if short_name is None else short_name
 
-    def add(self, instr_template):
+    def add(self, instr_template: InstructionTemplate):
         self.instruction_templates.append(instr_template)
         for instr in instr_template.instructions:
             self.instruction_set.add(instr)
+
+    def to_dict(self):
+        ret = {'name': self.name,
+               'short_name': self.short_name,
+               'templates': [t.to_dict() for t in self.instruction_templates]}
+        return ret
